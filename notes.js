e6df35ddcd8c03eccc1c7256f86cbcbd4b08aa87
boxybird <andrew.rhyand@gmail.com>
2020-11-18 07:59:30 -0800

    delete unused file

--- notes.js
+++ notes.js
@@ -1,18 +1,1 @@
-async login() {
-  try {
-    const { data } = await axios.post('/wp-json/simple-jwt-authentication/v1/token', {
-      username: 'jacelane',
-      password: 'ticklishsalamander'
-    })
 
-    const { data } = await axios.post('/wp-json/simple-jwt-authentication/v1/token', this.form)
-
-    this.user = data
-
-    this.form.username = ''
-    this.form.password = ''
-  } catch (error) {
-    console.log(error)
-    this.handleToast('Nope! Try again.')
-  }
-} 
